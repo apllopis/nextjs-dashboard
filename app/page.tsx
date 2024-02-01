36a185fd46e29f6ad3202fdea7282e44246a58f7
apllopis <alberto.pintadollopis@gmail.com>
2024-02-01 20:34:45 +0100

    cambiados colores

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-4">
       <div className="flex h-10 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-40">
-        {/* <AcmeLogo /> */}
-        <div className={styles.shape}>Locomotoras</div>
+        { <AcmeLogo /> }
+        {/*<div className={styles.shape}>Locomotoras</div>*/}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -28,8 +28,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* <img src='/hero-desktop.png' alt='Screenshot de hero-desktop' />
-          */}
           <Image 
             src='/hero-desktop.png' 
             alt='Screenshot de hero-desktop' 
